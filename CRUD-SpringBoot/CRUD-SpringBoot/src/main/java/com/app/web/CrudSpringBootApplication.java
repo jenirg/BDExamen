d3962f2ebfb0246a65557aa9682cff0b8bb8f842
jenirg <38708051+jenirg@users.noreply.github.com>
2023-07-03 19:15:44 -0600

    CarpetaAplicacion

---
+++ CRUD-SpringBoot/CRUD-SpringBoot/src/main/java/com/app/web/CrudSpringBootApplication.java
@@ -0,0 +1,26 @@
+package com.app.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.app.web.entidad.Estudiante;
+import com.app.web.repositorio.EstudianteRepositorio;
+
+@SpringBootApplication
+public class CrudSpringBootApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+	}
+
+	@Autowired
+	private EstudianteRepositorio repositorio;
+	
+	@Override
+	public void run(String... args) throws Exception {
+	
+	}
+
+}
---
+++ CRUD-SpringBoot/CRUD-SpringBoot/src/main/java/com/app/web/controlador/EstudianteControlador.java
@@ -0,0 +1,23 @@
+package com.app.web.controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.web.servicio.EstudianteServicio;
+
+@Controller
+public class EstudianteControlador {
+
+	@Autowired
+	private EstudianteServicio servicio;
+
+	@GetMapping({ "/estudiantes", "/" })
+	public String listarEstudiantes(Model modelo) {
+		modelo.addAttribute("estudiantes", servicio.listarTodosLosEstudiantes());
+		return "estudiantes"; 
+	}
+
+}
